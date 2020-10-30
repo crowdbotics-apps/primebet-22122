d95848d46b3dd533a96597856d9aade7b9114513
Crowdbotics <team@crowdbotics.com>
2020-10-30 15:07:23 +0000

    Update models

--- backend/primebet_22122/wsgi.py
+++ backend/primebet_22122/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primebet_22122.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "primebet_22122.settings")
 
 application = get_wsgi_application()
